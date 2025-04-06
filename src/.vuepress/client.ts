534b52f1361672f54a77822d2ec4f765912c132b
Wallbreaker5th <3277946283@qq.com>
2025-04-06 22:12:05 +0800

    Use Microsoft Clarity

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -1,16 +1,14 @@
 import { defineClientConfig } from "vuepress/client";
-import baiduAnalytics from "vue-baidu-analytics";
+import Clarity from '@microsoft/clarity';
 import TypstRenderer from "./components/TypstRenderer.vue";
+
 
 export default defineClientConfig({
   enhance({ app, router, siteData }) {
-    app.use(baiduAnalytics, {
-      router: router,
-      siteIdList: ["74813e89f42b6950a8264a139cb12fa2"],
-      isDebug: true,
-    });
     app.component("TypstRenderer", TypstRenderer);
   },
-  setup() {},
+  setup() {
+    Clarity.init("qzwl6o429v");
+  },
   rootComponents: [],
 });
